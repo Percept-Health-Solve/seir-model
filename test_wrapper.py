12d079538fb6b7943da3b98c2d9821a822009101
Jason Webster <jasonrobwebster@gmail.com>
2020-04-07 16:22:54 +0200

    added example of dict variables

--- test_wrapper.py
+++ test_wrapper.py
@@ -15,11 +15,12 @@ q_ii = np.array([
 
 infectious_func = lambda t: 1 if t <= 17 else 0.2 if 17 < t < 38 else 1
 imported_func = lambda t: [[0, 0.75 * 9 * np.exp(0.11*t), 0, 0], [0, 0.25 * 9 * np.exp(0.11*t), 5, 0]] if t < 16 else 0
+imported_func = lambda t: 1
 
 model = MultiPopWrapper(
     pop_categories={'age': ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+'],
                     'sex': ['male', 'female'],
-                    'risk': ['high', 'low']
+                    'risk': ['low', 'high']
                     },
     inf_labels=['AS', 'GP', 'H', 'ICU'],
     alpha=[0.179, 0.821, 0, 0.],
@@ -28,25 +29,27 @@ model = MultiPopWrapper(
     q_ii=q_ii,
     q_ir=[1/10, 1/2.3, 1/8, 1/10],
     q_id=[0, 0, 0, 1/5],
-    delta=[0, 0.012, 0.05, 0],
-    beta=[0, 0, 0, 0.609],
+    delta={'20-29': [0, 0.012, 0.05, 0],
+           '60-69': [0, 0.166, 0.274, 0]},
+    beta={'20-29': [0, 0.012, 0.05, 0],
+          '60-69': [0, 0.166, 0.274, 0]},
     infectious_func=infectious_func,
     imported_func=imported_func,
     extend_vars=True
 )
 
-print(model.pop_categories)
+print(model.beta)
 print(model.nb_groups)
 print(model.pop_labels)
-print(model.pop_label_to_idx)
 
 init_vectors = {
-    's_0': [27000000, 8000000]
+    's_0': {'20-29_male_low': 29000000,
+            '60-69_male_low': 8000000}
 }
-t = np.linspace(0, 300, 10000)
+
+t = np.linspace(0, 300, 1501)
 solution = model.solve(init_vectors, t, to_csv=True, fp='data/solution.csv')
 
-print(model.r_0_eff)
 print(model.r_0)
 
 # plot all figures
@@ -57,7 +60,7 @@ fig, axes = plot_solution(solution, t)
 #         ax.set_xlim((0, 50))
 #         ax.set_ylim((0, 50))
 
-plt.show()
+# plt.show()
 
 # plot young
 # fig, axes = plot_solution(solution, t, 0)
