2af5aabca205f8da4fb68e7738d5f02316437ca3
Jason Webster <jasonrobwebster@gmail.com>
2020-04-08 17:03:30 +0200

    Updated to six state model.

--- test_wrapper.py
+++ test_wrapper.py
@@ -5,14 +5,6 @@ from seir.model import NInfectiousModel
 from seir.wrapper import MultiPopWrapper
 from seir.utils import plot_solution
 
-q_ii = np.array([
-    [0, 0, 0, 0],
-    [0, 1 / 5, 0, 0],
-    [0, -1 / 5, 1 / 6, 0],
-    [0, 0, -1 / 6, 0]
-])
-# q_ii = np.repeat(np.expand_dims(q_ii, 0), 2, axis=0)
-
 infectious_func = lambda t: 1 if t <= 17 else 0.2 if 17 < t < 38 else 1
 # imported_func = lambda t: [[0, 0.75 * 9 * np.exp(0.11*t), 0, 0], [0, 0.25 * 9 * np.exp(0.11*t), 5, 0]] if t < 16 else 0
 imported_func = None
@@ -22,36 +14,50 @@ model = MultiPopWrapper(
                     'sex': ['male', 'female'],
                     'risk': ['low', 'high']
                     },
-    inf_labels=['AS', 'GP', 'H', 'ICU'],
-    alpha=[0.179, 0.821, 0, 0.],
+    inf_labels=['AS', 'M', 'S', 'SI', 'H', 'ICU'],
+    alpha={'0-9': [0.179, 0.821 * 0.999, 0.821 * 0.001, 0, 0, 0],
+           '10-19': [0.179, 0.821 * 0.997, 0.821 * 0.003, 0, 0, 0],
+           '20-29': [0.179, 0.821 * 0.988, 0.821 * 0.012, 0, 0, 0],
+           '30-39': [0.179, 0.821 * 0.968, 0.821 * 0.032, 0, 0, 0],
+           '40-49': [0.179, 0.821 * 0.951, 0.821 * 0.049, 0, 0, 0],
+           '50-59': [0.179, 0.821 * 0.898, 0.821 * 0.102, 0, 0, 0],
+           '60-69': [0.179, 0.821 * 0.834, 0.821 * 0.166, 0, 0, 0],
+           '70-79': [0.179, 0.821 * 0.757, 0.821 * 0.243, 0, 0, 0],
+           '80+': [0.179, 0.821 * 0.727, 0.821 * 0.273, 0, 0, 0]},
     t_inc=5.1,
-    q_se=[0.4163, 0.8326, 0, 0],
-    q_ii=q_ii,
-    q_ir=[1/10, 1/2.3, 1/8, 1/10],
-    q_id=[0, 0, 0, 1/5],
-    delta={'20-29': [0, 0.012, 0.05, 0],
-           '60-69': [0, 0.166, 0.274, 0]},
-    beta={'20-29': [0, 0, 0, 0.609],
-          '60-69': [0, 0, 0, 0.589]},
+    q_se=[0.4163, 0.8326, 0.8326, 0, 0, 0],
+    q_ii=[
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1/2.3, 0, 0, 0],
+        [0, 0, -1/2.3, 1/2.7, 0, 0],
+        [0, 0, 0, -1/2.7, 1/6, 0],
+        [0, 0, 0, 0, -1/6, 0]
+    ],
+    q_ir=[1 / 10, 1 / 2.3, 0, 0, 1 / 8, 1 / 10],
+    q_id=[0, 0, 0, 0, 0, 1 / 5],
+    rho_delta={'20-29': [0, 0, 1, 1, 0.05, 0],
+               '60-69': [0, 0, 1, 1, 0.274, 0]},
+    rho_beta={'20-29': [0, 0, 0, 0, 0, 0.609],
+              '60-69': [0, 0, 0, 0, 0, 0.589]},
     infectious_func=infectious_func,
     imported_func=imported_func,
     extend_vars=True
 )
 
-print(model.beta)
-print(model.nb_groups)
-print(model.pop_labels)
-
 init_vectors = {
     's_0': {'20-29_male_low': 27000000,
             '60-69_male_low': 8000000},
-    'i_0': {'20-29_male_low': [0, 100, 0, 0]}
+    'i_0': {'20-29_male_low': [0, 100, 0, 0, 0, 0]}
 }
 
 t = np.linspace(0, 300, 1501)
 solution = model.solve(init_vectors, t, to_csv=True, fp='data/solution.csv')
 
+
 print(model.r_0)
+
+s_t, e_t, i_t, r_t, d_t = solution
 
 # plot all figures
 fig, axes = plot_solution(solution, t)
@@ -70,5 +76,3 @@ plt.show()
 # # plot old
 # fig, axes = plot_solution(solution, t, 1)
 # plt.show()
-
-
