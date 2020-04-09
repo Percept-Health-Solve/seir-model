73e756efc72999715c5f1eca519e40417fa64541
Jason Webster <jasonrobwebster@gmail.com>
2020-04-09 18:24:07 +0200

    added ability to plot detected cases

--- seir/utils.py
+++ seir/utils.py
@@ -9,7 +9,7 @@ mpl.rcParams['figure.figsize'] = (13, 8)
 mpl.rcParams['figure.dpi'] = 100
 
 
-def plot_solution(solution, t, group=None, show_cumulative=False):
+def plot_solution(solution, t, group=None, show_cumulative=False, show_detected=True):
     s_t, e_t, i_t, r_t, d_t = solution
     if group is None:
         fig, axes = plt.subplots(2, 2, sharex=True)
@@ -18,13 +18,20 @@ def plot_solution(solution, t, group=None, show_cumulative=False):
             axes[0, 0].plot(
                 t,
                 np.sum(i_t[:, :, 1:] + r_t[:, :, 1:] + d_t[:, :, 1:], axis=(1, 2)),
-                label='Cumulative Infections'
+                label='Cumulative Symptomatic Infections'
+            )
+        if show_detected:
+            axes[0, 0].plot(
+                t,
+                0.6 * np.sum(i_t[:, :, 1] + r_t[:, :, 1] + d_t[:, :, 1], axis=-1)
+                + np.sum(i_t[:, :, 2:] + r_t[:, :, 2:] + d_t[:, :, 2:], axis=(1, 2)),
+                label='Detected Symptomatic Infections'
             )
         axes[0, 0].plot(t, np.sum(i_t, axis=(1, 2)), label='Total Infected')
         axes[0, 0].plot(t, np.sum(i_t[:, :, 0], axis=-1), label='Infectious Asymptomatic')
         axes[0, 0].plot(t, np.sum(i_t[:, :, 1], axis=-1), label='Infectious Mild')
         axes[0, 0].plot(t, np.sum(i_t[:, :, 2], axis=-1), label='Infectious Severe')
-        axes[0, 0].plot(t, np.sum(i_t[:, :, 3:], axis=(1, 2)), label='Infectious Severe (I/H/ICU)')
+        axes[0, 0].plot(t, np.sum(i_t[:, :, 3:], axis=(1, 2)), label='Isolated Severe (I/H/ICU)')
         axes[0, 0].set_ylabel("Infections")
         axes[0, 0].legend()
 
@@ -34,10 +41,10 @@ def plot_solution(solution, t, group=None, show_cumulative=False):
         axes[0, 1].set_ylabel("Serious cases")
         axes[0, 1].legend()
 
-        axes[1, 0].plot(t, np.sum(r_t, axis=(1, 2)), label='Recovered Total')
-        axes[1, 0].plot(t, np.sum(r_t[:, :, 0], axis=-1), label='Recovered Asymptomatic')
-        axes[1, 0].plot(t, np.sum(r_t[:, :, 1], axis=-1), label='Recovered Mild')
-        axes[1, 0].plot(t, np.sum(r_t[:, :, 2:], axis=(1, 2)), label='Recovered Severe')
+        axes[1, 0].plot(t, np.sum(r_t, axis=(1, 2)), label='Isolated Total')
+        axes[1, 0].plot(t, np.sum(r_t[:, :, 0], axis=-1), label='Isolated Asymptomatic')
+        axes[1, 0].plot(t, np.sum(r_t[:, :, 1], axis=-1), label='Isolated Mild')
+        axes[1, 0].plot(t, np.sum(r_t[:, :, 2:], axis=(1, 2)), label='Isolated Severe')
         axes[1, 0].set_xlabel('Days')
         axes[1, 0].set_ylabel("Recoveries")
         axes[1, 0].legend()
